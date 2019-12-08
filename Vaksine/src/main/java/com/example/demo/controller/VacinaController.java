00eb980681192e62eff5f856808990c091fa356d
gabriel lima <gabriel.luiz.g.lima@accenture.com>
2019-12-08 20:45:54 -0300

    alteracoes

--- Vaksine/src/main/java/com/example/demo/controller/VacinaController.java
+++ Vaksine/src/main/java/com/example/demo/controller/VacinaController.java
@@ -7,14 +7,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.dao.VacinaDAO;
+import com.example.demo.model.Vacina;
 
 @Controller
 public class VacinaController {
 	
 	@Autowired
-	private VacinaDAO vacinaDAO; 
+	private VacinaDAO vacinaDAO;
+	
+	@GetMapping("/vacinaForm")
+	public String exibirForm() {
+		return "cadastro-vacina";
+	}
+	
+	@PostMapping("/cadastrarVacina")
+	public String cadastrarVacina(Vacina vacina) {
+		vacinaDAO.save(vacina);
+		return "cadastro-vacina";
+	}
 	
 	@GetMapping("/editarVacina")
 	public String atualizarVacina(@ Valid BindingResult atributes, Model model, Integer id) {
