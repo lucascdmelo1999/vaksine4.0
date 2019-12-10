7cebb30963eedee678cb1bf31c0a3edfb9db9450
lucascdmelo1999 <lukast.j.s@hotmail.com>
2019-12-10 00:16:04 -0200

    alterações em buscar vacina(Back)

--- Vaksine/src/main/java/com/example/demo/controller/VacinaController.java
+++ Vaksine/src/main/java/com/example/demo/controller/VacinaController.java
@@ -10,45 +10,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.dao.VacinaDAO;
-import com.example.demo.model.Usuario;
 import com.example.demo.model.Vacina;
 
 @Controller
 public class VacinaController {
-	
+
 	@Autowired
 	private VacinaDAO vacinaDAO;
-	
+
 	@GetMapping("/vacinaform")
 	public String exibirForm(Vacina vacina) {
 		return "cadastro-vacina";
 	}
-	
+
 	@GetMapping("/vacinalist")
 	public String listarVacina(Vacina vacina) {
-		
+
 		return "lista-vacina";
 	}
+
+	@GetMapping("/vacinabuscar")
+	public String buscarvacina(Vacina vacina, Model model) {
+		model.addAttribute("vacina", vacinaDAO.findAll());
+		return "redirect:/vacinalist";
+	}
+
 	@PostMapping("/cadastrarvacina")
 	public String cadastrarVacina(Vacina vacina, Model model) {
 		model.addAttribute("vacina", vacina);
 		vacinaDAO.save(vacina);
 		return "redirect:/vacinalist";
 	}
-	
+
 	@GetMapping("/editarvacina")
-	public String atualizarVacina(@ Valid BindingResult atributes, Model model, Integer id) {
+	public String atualizarVacina(@Valid BindingResult atributes, Model model, Integer id) {
 		model.addAttribute("listaDados", vacinaDAO.findById(id));
 		return "vacina-form";
 	}
-	
+
 	@GetMapping("/deletarvacina")
 	public String deletarVacina(Integer id) {
 		vacinaDAO.deleteById(id);
 		return "redirect:/listavacina";
-	
+
 	}
-	
+
 	@GetMapping("/listarvacinas")
 	public String listarVacinas(Model model) {
 		model.addAttribute("listaVacinas", vacinaDAO.findAll());
--- Vaksine/src/main/java/com/example/demo/model/Posto.java
+++ Vaksine/src/main/java/com/example/demo/model/Posto.java
@@ -1,10 +1,8 @@
 package com.example.demo.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Posto {
