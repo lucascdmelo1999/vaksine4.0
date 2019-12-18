d4c6c1892abd744a6bc487e29febbf56227fc9ac
lucascdmelo <lukast.j.s@hotmail.com>
2019-12-18 19:33:05 -0300

    finalização da pagina buscar vacina

--- Vaksine/src/main/java/com/example/demo/controller/VacinaController.java
+++ Vaksine/src/main/java/com/example/demo/controller/VacinaController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
@@ -15,12 +17,12 @@ public class VacinaController {
 
 	@Autowired
 	private VacinaDAO vacinaDAO;
-	
+
 	@GetMapping("/vacinaform")
 	public String exibirForm(Vacina vacina) {
 		return "/cadastro-vacina";
 	}
-	
+
 	@GetMapping("/vacinacaoDeletar")
 	public String vacinacaoDeletar(Vacina vacina) {
 		return "/vacinacao-deletar";
@@ -28,19 +30,26 @@ public class VacinaController {
 
 	@GetMapping("/vacinalist")
 	public String listarVacina(Vacina vacina, Model model) {
-		model.addAttribute("listaVacinas",this.vacinaDAO.findAll(Sort.by("id")));
+		model.addAttribute("listaVacinas", this.vacinaDAO.findAll(Sort.by("id")));
 		return "/lista-vacina3";
 	}
 
 	@GetMapping("/vacinabuscar")
 	public String buscarvacina(Vacina vacina, Model model) {
-		model.addAttribute("vacina", vacinaDAO.findAll());
+		return "buscarvacina";
+	}
+
+	@PostMapping("/pesquisarvacina")
+	public String pesquisarvacina(Vacina vacina, Model model) {
+										// select nome from vacina where nome LIKE %nome%;
+		List<Vacina> resultado = this.vacinaDAO.findByNomeContainingIgnoreCase(vacina.getNome(), Sort.by("nome"));
+		model.addAttribute("listaVacinas", resultado);
 		return "buscarvacina";
 	}
 
 	@PostMapping("/vacinaCadastro")
 	public String cadastrarVacina(Vacina vacina) {
-		this.vacinaDAO.save(vacina); 
+		this.vacinaDAO.save(vacina);
 		return "redirect:/vacinalist";
 	}
 
@@ -56,16 +65,10 @@ public class VacinaController {
 		return "redirect:/vacinalist";
 
 	}
-	
-	
-	
-	
-	
-	
-/*
-	@GetMapping("/listarvacinas")
-	public String listarVacinas(Model model) {
-		model.addAttribute("listaVacinas",this.vacinaDAO.findAll(Sort.by("id")));
-		return "redirect:/vacinalist";
-	}*/
+
+	/*
+	 * @GetMapping("/listarvacinas") public String listarVacinas(Model model) {
+	 * model.addAttribute("listaVacinas",this.vacinaDAO.findAll(Sort.by("id")));
+	 * return "redirect:/vacinalist"; }
+	 */
 }
--- Vaksine/src/main/java/com/example/demo/dao/VacinaDAO.java
+++ Vaksine/src/main/java/com/example/demo/dao/VacinaDAO.java
@@ -1,5 +1,8 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import com.example.demo.model.Vacina;
 
 @Repository
 public interface VacinaDAO extends JpaRepository<Vacina, Integer>{
+	
+	List<Vacina> findByNomeContainingIgnoreCase(String nome, Sort sort);
 
 }
