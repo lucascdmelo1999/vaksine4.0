1b46c2b1579fe8aa8159bee73ee899d10438871a
edesildo <edesildo@gmail.com>
2019-12-18 20:45:19 -0300

    Aparecendo na parte de cadastro cadastro realizado com sucesso

--- Vaksine/src/main/java/com/example/demo/controller/CadastroUsuarioController.java
+++ Vaksine/src/main/java/com/example/demo/controller/CadastroUsuarioController.java
@@ -3,9 +3,10 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.dao.UsuarioDAO;
 import com.example.demo.model.Usuario;
@@ -24,10 +25,18 @@ public class CadastroUsuarioController {
 	}
 
 	@PostMapping("/cad")
-	public String cadastrar(Usuario usuario) {
+	public String cadastrar(Usuario usuario,BindingResult result, RedirectAttributes redirectAttributes) {
 		usuarioDAO.save(usuario);
+		
+		redirectAttributes.addFlashAttribute("message", "Failed");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+		if (result.hasErrors()) {
+			return "redirect:/cadUsuario";
+		}
+		redirectAttributes.addFlashAttribute("message", "Success");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
 		return "redirect:/cadUsuario";
-
+		
 	}
 
 }
