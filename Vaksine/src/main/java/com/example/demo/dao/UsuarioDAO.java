826ed60e743b1d8be856e344edd277cd84656655
gabriel lima <gabriel.luiz.g.lima@accenture.com>
2020-01-31 22:36:17 -0300

    ajuste bug cadastro usuario

--- Vaksine/src/main/java/com/example/demo/dao/UsuarioDAO.java
+++ Vaksine/src/main/java/com/example/demo/dao/UsuarioDAO.java
@@ -2,6 +2,7 @@ package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Usuario;
@@ -14,19 +15,19 @@ public interface UsuarioDAO extends JpaRepository<Usuario, Integer> {
 	
 	
 	@Query("select c from Usuario c where c.email = :email and c.senha = :senha")
-	public  Usuario UsuarioLogin(String email, String senha);
+	public  Usuario UsuarioLogin(@Param("email") String email, @Param("senha") String senha);
 	
 	@Query("select c from Usuario c where c.email = :email and c.email <> :email")
-	public Usuario findByEmailIgnoreCase(String email);
+	public Usuario findByEmailIgnoreCase(@Param("email") String email);
 	
 	@Query("select c from Usuario c where c.email = :email")
-	public Usuario verificacaoEmail(String email);
+	public Usuario verificacaoEmail(@Param("email") String email);
 	
 	@Query("select c from Usuario c where c.cpf = :cpf and c.cpf <> :cpf")
-	public Usuario findByCpfIgnoreCase(String cpf);
+	public Usuario findByCpfIgnoreCase(@Param("cpf") String cpf);
 	
 	@Query("select c from Usuario c where c.id = :id")
-	public Usuario findByid(Integer id);
+	public Usuario findByid(@Param("id") Integer id);
 
 
 }
