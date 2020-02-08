c88a8d0c82807622d1dd3ce446a591bcb20e2541
Wesley Oliveira <wesleyoliveira11@gmail.com>
2020-02-08 07:54:42 -0200

    front de cartao de vacina e apagando vacina com ajax(falta atualizar a pagina)

--- Vaksine/src/main/java/com/example/demo/dao/UsuarioDAO.java
+++ Vaksine/src/main/java/com/example/demo/dao/UsuarioDAO.java
@@ -31,7 +31,7 @@ public interface UsuarioDAO extends JpaRepository<Usuario, Integer> {
 	
 	@Query("select c from Usuario c where c.id = :id")
 	public Usuario findByid(@Param("id") Integer id);
-	@Query("")
-	public Usuario editarPerfil();
-	
+//	@Query("")
+//	public Usuario editarPerfil();
+//	
 }
